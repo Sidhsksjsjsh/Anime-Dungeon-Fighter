cf02cd57d63869725eb20f9f9f0e178aacdd286b
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-01-29 17:48:12 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,6 +1,8 @@
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/VAPE-UI-MODDED/main/.lua"))()
 local wndw = lib:Window("VIP Turtle Hub V4 - REQUEST ANY FEATURE")
 local T1 = wndw:Tab("Main")
+local T2 = wndw:Tab("Server Manipulator")
+local T3 = wndw:Tab("Draw")
 
 local vis = {
   a = 0,
@@ -10,6 +12,13 @@ local vis = {
   e = 0,
   f = ""
 }
+local draw = {
+  a = 7000000
+}
+
+T2:Button("Infinite Coins",function()
+    game:GetService("ReplicatedStorage")["Msg"]["DrawWeapon"]:InvokeServer(7000003,-math.huge)
+end)
 
 T1:Toggle("Auto kill V1 [ Hit the enemy first ]",false,function(value)
     _G.killv1 = value
@@ -19,8 +28,28 @@ T1:Toggle("Auto kill V1 [ Hit the enemy first ]",false,function(value)
     end
 end)
 
-T1:Toggle("Auto kill V2 [ Not Working ]",false,function(value)
-    _G.killv2 = value
+T3:Toggle("Auto draw fruit [ X1 ]",false,function(value)
+    _G.df = value
+    while wait() do
+      if _G.df == false then break end
+      game:GetService("ReplicatedStorage")["Msg"]["DrawFruit"]:InvokeServer(1)
+    end
+end)
+
+T3:Toggle("Auto draw fruit [ X10 ]",false,function(value)
+    _G.dfn = value
+    while wait() do
+      if _G.dfn == false then break end
+      game:GetService("ReplicatedStorage")["Msg"]["DrawFruit"]:InvokeServer(10)
+    end
+end)
+
+T3:Toggle("Auto draw weapon [ Draw before enabling this ]",false,function(value)
+    _G.dw = value
+    while wait() do
+      if _G.dw == false then break end
+      game:GetService("ReplicatedStorage")["Msg"]["DrawWeapon"]:InvokeServer(draw.a,1)
+    end
 end)
 
 lib:HookFunction(function(method,self,args)
@@ -31,6 +60,8 @@ lib:HookFunction(function(method,self,args)
       vis.d = args[1]["hitID"]
       vis.e = args[1]["skillID"]
       vis.f = args[2]
+    elseif self == "DrawWeapon" and method == "InvokeServer" then
+      draw.a = args[1]
     end
 end)
 
